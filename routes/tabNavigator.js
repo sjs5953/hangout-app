46dffb78bf06661e293ad0a75977a15c851ade39
Jay Seo <sjs5953@hotmail.com>
2020-04-01 16:43:27 -0700

    added icons and labels for tabs

--- routes/tabNavigator.js
+++ routes/tabNavigator.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// '@react-navigation/material-bottom-tabs';
+import { AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons';
 
 
 import AuthStackScreen from './stacks/authStack'
@@ -17,10 +18,22 @@ export default function TabNavigator ({ userToken, setUserToken }) {
   <NavigationContainer>
     {userToken ? 
       <Tabs.Navigator>
-        <Tabs.Screen name='EventsStack' component={EventsStackScreen}/>
-        <Tabs.Screen name='CreateStack' component={CreateStackScreen}/>
-        <Tabs.Screen name='NotificationsStack' component={NotificationsStackScreen}/>
-        <Tabs.Screen name='UserStack' component={UserStackScreen}/>
+        <Tabs.Screen name='EventsStack' component={EventsStackScreen} labeled={false} options={{
+          tabBarLabel:"Home",
+          tabBarIcon: ({color, focused}) => <AntDesign name="home" color={color} size={27} />
+        }}/>
+        <Tabs.Screen name='CreateStack' component={CreateStackScreen} options={{
+          tabBarLabel:"Create",
+          tabBarIcon: () => <FontAwesome name="plus-square-o" size={27} />
+        }}/>
+        <Tabs.Screen name='NotificationsStack' component={NotificationsStackScreen} options={{
+          tabBarLabel:"Notification",
+          tabBarIcon: () => <Ionicons name="ios-notifications-outline" size={27} />
+        }}/>
+        <Tabs.Screen name='UserStack' component={UserStackScreen} options={{
+          tabBarLabel:"Profile",
+          tabBarIcon: () => <AntDesign name="user" size={27} />
+        }}/>
       </Tabs.Navigator>
       : <AuthStackScreen />
     }
