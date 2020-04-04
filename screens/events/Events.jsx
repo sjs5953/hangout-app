6d56fcf5b910f962542cf66a0f0b566f78755bc8
Jay Seo <sjs5953@hotmail.com>
2020-04-03 21:15:09 -0700

    added toggle button to switch between map and list

--- screens/events/Events.jsx
+++ screens/events/Events.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, Text, View, FlatList,TouchableOpacity } from 'react-native'
 import {globalStyles} from '../../styles/global'
 import debounce from'lodash.debounce';
 import Card from '../../shared/Card'
+import { RoundButton } from '../../shared/Button';
+import { AntDesign, FontAwesome5, Ionicons, Foundation, Fontisto } from '@expo/vector-icons';
 
 
 const events = [
@@ -14,30 +16,60 @@ const events = [
 ]
 
 const Events = ({navigation}) => {
+
+  // ListView = false, MapView = true
+  const [listView, setlistView] = useState(true);
+
+  const toggleView = () => {
+    setlistView(!listView);
+  }
+
   return (
     <View style={globalStyles.container}>
-      <FlatList
-        data={events}
-        renderItem={({item})=>(
-          <TouchableOpacity style={globalStyles.titleText} onPress={debounce(
-            () =>
-            navigation.push('Event', {item}), 500,{
-              'leading': true,
-              'trailing': false
-            }
-          )}>
-            <Card>
-              <Text style={globalStyles.titleText}>
-                {item.title}
-              </Text>
-            </Card>
-          </TouchableOpacity>
-        )}
-      />
+      {listView? 
+         <FlatList
+         data={events}
+         renderItem={({item})=>(
+           <TouchableOpacity style={globalStyles.titleText} onPress={debounce(
+             () =>
+             navigation.push('Event', {item}), 500,{
+               'leading': true,
+               'trailing': false
+             }
+           )}>
+             <Card>
+               <Text style={globalStyles.titleText}>
+                 {item.title}
+               </Text>
+             </Card>
+           </TouchableOpacity>
+         )}
+       />
+        :
+        <Text style={globalStyles.titleText}>MapView!</Text>
+        }
+     
+
+    {/* a component that needs to stay for all screens inside events screen */}
+      <View style={styles.toggleContainer}>
+        <RoundButton style={styles.toggleButton} onPress={toggleView}>
+          {listView?
+            <FontAwesome5 name="map" color={"black"} size={30} /> :
+            <FontAwesome5 name="list-ul" color={"black"} size={30} />}
+        </RoundButton>
+      </View>
     </View>
   )
 }
 
 export default Events
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  toggleContainer:{
+    position:'absolute',
+    bottom:20,
+    right:25,
+    flexDirection:'row',
+    justifyContent:'flex-end'
+  },
+})
--- shared/Button.jsx
+++ shared/Button.jsx
@@ -1,32 +1,71 @@
 import React from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
-const FlatButton = ({text, onPress}) => {
+export const FlatButton = ({text, onPress}) => {
   
   return (
     <TouchableOpacity onPress={onPress}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}> { text } </Text>
+      <View style={styles.flatButton}>
+        <Text style={styles.faltButtonText}> { text } </Text>
       </View>
     </TouchableOpacity>
   )
 }
 
-export default FlatButton
+export const RoundButton = ({text, onPress, children}) => {
+  
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.roundButton}>
+        <Text style={styles.roundButtonText}> { children } </Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
 
 const styles = StyleSheet.create({
-  button: {
+  flatButton: {
     borderRadius:8,
     paddingVertical: 14,
     paddingHorizontal: 10,
     backgroundColor: '#f01d71'
   },
-  buttonText: {
+  flatButtonText: {
     color:'white',
     fontWeight: 'bold',
     textTransform:'uppercase',
     fontSize:16,
     textAlign:'center'
-  }
+  },
+  roundButton: {
+    borderRadius:100,
+    paddingVertical: 14,
+    paddingHorizontal: 10,
+    // backgroundColor: '#f01d71',
+    height:50,
+    width:80,
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth:0.1,
+    borderStyle:'solid',
+    borderColor:"#333",
+    
+    elevation:3,
+    backgroundColor:'#fff',
+    shadowOffset: {
+      width:1,
+      height:1
+    },
+    shadowRadius:2,
+  },
+  roundButtonText: {
+    color:'#333',
+    fontWeight: 'bold',
+    textTransform:'uppercase',
+    fontSize:16,
+    textAlign:'center'
+  },
+ 
 })
 
